90370ca39c50faf4423df0af4f575da2ccbe5f06
Daniel Ducuara <daniel.ducuara@mail.escuelaing.edu.co>
2021-06-22 16:46:43 -0500

    Code Source

--- src/test/java/edu/escuelaing/arsw/springWeb/SpringWebApplicationTests.java
+++ src/test/java/edu/escuelaing/arsw/springWeb/SpringWebApplicationTests.java
@@ -29,6 +29,19 @@ class SpringWebApplicationTests {
 	 * this test verifies that the program returns the correct number of picas
 	 */
 	@Test
+	public void testFamas2() {
+		Game game = new Game();
+		int nums[] = {1,2,3,4};
+		int numsUser[] = {4,2,3,1};
+		game.setNumbers(nums);
+		game.numbers(numsUser);
+		assertEquals(2,game.getFamas());
+	}
+	
+	/**
+	 * this test verifies that the program returns the correct number of picas
+	 */
+	@Test
 	public void testPicas() {
 		Game game = new Game();
 		int nums[] = {1,2,3,4};
@@ -36,6 +49,32 @@ class SpringWebApplicationTests {
 		game.setNumbers(nums);
 		game.numbers(numsUser);
 		assertEquals(2,game.getPicas());
+	}
+	
+	/**
+	 * this test verifies that the program returns the correct number of picas
+	 */
+	@Test
+	public void testPicas2() {
+		Game game = new Game();
+		int nums[] = {1,2,3,4};
+		int numsUser[] = {1,2,3,4};
+		game.setNumbers(nums);
+		game.numbers(numsUser);
+		assertEquals(4,game.getPicas());
+	}
+	
+	/**
+	 * this test verifies that the program returns the correct number of picas
+	 */
+	@Test
+	public void testWin() {
+		Game game = new Game();
+		int nums[] = {1,2,3,4};
+		int numsUser[] = {1,2,3,4};
+		game.setNumbers(nums);
+		game.numbers(numsUser);
+		assertEquals(true,game.getWin());
 	}
 	
 }
