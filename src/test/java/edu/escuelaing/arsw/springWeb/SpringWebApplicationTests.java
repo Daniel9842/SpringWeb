f5aa5a469b287f65ce27f551aae2350d71f4e572
Daniel Ducuara <daniel.ducuara@mail.escuelaing.edu.co>
2021-06-22 11:54:44 -0500

    version2

--- src/test/java/edu/escuelaing/arsw/springWeb/SpringWebApplicationTests.java
+++ src/test/java/edu/escuelaing/arsw/springWeb/SpringWebApplicationTests.java
@@ -3,11 +3,13 @@ package edu.escuelaing.arsw.springWeb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import edu.escuelaing.arsw.springWeb.picasYfamas.Game;
+
 @SpringBootTest
 class SpringWebApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public void testPicas() {
+		Game game = new Game();
 	}
 
 }
